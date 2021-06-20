e8afae235cd1f768224762ba3f9240aed6ea12a0
Sreejith Gopinathan <jithappan@gmail.com>
2021-06-20 10:53:24 +0530

    Adding script to check Cross Origin Resource Sharing

--- GitHubCICDTestApp/wwwroot/js/site.js
+++ GitHubCICDTestApp/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function GetCourses() {
+    var _url = 'https://az-204-lab-course-api.azurewebsites.net/api/Course';
+
+    $.ajax({
+        url: _url,
+        type: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
